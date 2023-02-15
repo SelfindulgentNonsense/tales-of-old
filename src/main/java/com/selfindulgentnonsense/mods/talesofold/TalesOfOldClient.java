c5fc7090047e906410eb1b88c98b2ff1cc7d1a2c
SelfindulgentNonsense <github@f.s6t.de>
2023-02-15 18:54:37 +0100

    Initial Commit

---
+++ src/main/java/com/selfindulgentnonsense/mods/talesofold/TalesOfOldClient.java
@@ -0,0 +1,10 @@
+package com.selfindulgentnonsense.mods.talesofold;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TalesOfOldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/selfindulgentnonsense/mods/talesofold/TalesOfOldDataGenerator.java
@@ -0,0 +1,11 @@
+package com.selfindulgentnonsense.mods.talesofold;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TalesOfOldDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
